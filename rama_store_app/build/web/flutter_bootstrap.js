c9ec9a8c75b67f0ecf517871f4072d3b9d936ee9
Ankit Sharma <asharma18.hit@gmail.com>
2026-07-29 16:24:12 +0530

    add Firebase Phone Auth implementation for 10k free SMS/month

--- rama_store_app/build/web/flutter_bootstrap.js
+++ rama_store_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "589030177" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3454556204" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
